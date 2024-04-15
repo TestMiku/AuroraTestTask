fc6a19da3f8e706f14e9c8e1efca129774402a94
MIKU-7437 <nurshanov.dias@gmail.com>
2024-04-15 10:49:50 +0500

    add/Edit%Delete

--- t2/api/documents/urls.py
+++ t2/api/documents/urls.py
@@ -4,4 +4,5 @@ from .views import DocumentListCreateAPIView, DocumentDetailUpdateDeleteAPIView
 urlpatterns = [
     path('', DocumentListCreateAPIView.as_view(), name='document_list_create'),
     path('<int:pk>/', DocumentDetailUpdateDeleteAPIView.as_view(), name='document_detail'),
+    path('<int:pk>/view/', DocumentDetailUpdateDeleteAPIView.as_view(), name='document_view'),
 ]
--- t2/api/documents/views.py
+++ t2/api/documents/views.py
@@ -12,7 +12,31 @@ class DocumentListCreateAPIView(generics.ListCreateAPIView):
         serializer.save(user=self.request.user)
 
 
+from django.http import FileResponse
+from django.shortcuts import get_object_or_404
+
 class DocumentDetailUpdateDeleteAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
     # permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        # Логика для просмотра документа
+        # Например, вы можете вернуть содержимое файла или другую информацию о документе
+        # В этом примере я предполагаю, что в модели Document есть поле file, содержащее путь к файлу
+        file_path = instance.file.path
+        return FileResponse(open(file_path, 'rb'))
+
+    def download(self, request, *args, **kwargs):
+        instance = self.get_object()
+        # Логика для скачивания документа
+        # В этом примере я также предполагаю, что в модели Document есть поле file, содержащее путь к файлу
+        file_path = instance.file.path
+        response = FileResponse(open(file_path, 'rb'))
+        response['Content-Disposition'] = f'attachment; filename="{instance.file.name}"'
+        return response
+
